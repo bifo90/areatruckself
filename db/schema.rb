67396045ed46b204de2feaf3ac4343a24354c876
Stefano <bifo90web@gmail.com>
2020-03-15 18:03:34 +0100

    scaffolding contents

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_124803) do
+ActiveRecord::Schema.define(version: 2020_03_15_170026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contents", force: :cascade do |t|
+    t.string "titolo"
+    t.text "descrizione"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
